12f1cf922a8589ad88939f8d46b3463abb68da06
yutarou12 <52701144+yutarou12@users.noreply.github.com>
2021-03-03 10:12:27 +0900

    [Fix] event検知のfunctionを削除

--- index.js
+++ index.js
@@ -91,13 +91,4 @@ client.on('message', async message => {
 
 });
 
-fs.readdir(`./events/`, (err, files) => {
-    if (err) return console.error(err);
-    files.forEach(file => {
-        let eventFunction = require(`./events/${file}`);
-        let eventName = file.split(".")[0];
-        client.on(eventName, (...args) => eventFunction.run(client, ...args));
-    });
-});
-
 client.login(config.TOKEN);
